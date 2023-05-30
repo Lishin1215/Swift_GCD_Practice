f482966463d0f5062981703051e18dd087093a11
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-05-30 23:41:25 +0800

    build new project & add git ignore file

---
+++ DI Part 3 - GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI Part 3 - GCD
+//
+//  Created by 簡莉芯 on 2023/5/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
